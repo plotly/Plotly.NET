9dcdbafafe2fe94f883a44eb17d7ef0c27b5050d
Timo Mühlhaus <muehlhaus@outlook.com>
2016-06-08 23:35:57 +0200

    Doc: Heatmaps

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -7,7 +7,7 @@
 FSharp.Plotly
 ======================
 
-The library FSharp.Plotly implements charting suitable for use from F# scripting. Once you load the library as followed, you can use the members of the 'Chart' type to easily build charts.
+The library FSharp.Plotly implements charting suitable for use from F# scripting. Once you load the library as followed, you can use the members of the `Chart` type to easily build charts.
 
 FSharp.Plotly is powered by popular JavaScript charting library [Plotly](https://plot.ly/). The library provides a complete mapping for the configuration options of the underlying library but empowers you to use the comfortable style known from the beautiful library [F# Charting](http://fslab.org/FSharp.Charting/). So you get a nice F# interface support with the full power of Plotly.
 *)
@@ -23,7 +23,7 @@ The following example creates a combined point and line chart:
 *)
 let x  = [1.; 2.; 3.; 4.; 5.; 6.; 7.; 8.; 9.; 10.; ]
 let y  = [5.; 2.5; 5.; 7.5; 5.; 2.5; 7.5; 4.5; 5.5; 5.]
-let y' = [2.; 1.5; 5.; 1.5; 3.; 2.5; 2.5; 1.5; 3.5; 1.] 
+let y' = [2.; 1.5; 5.; 1.5; 3.; 2.5; 2.5; 1.5; 3.5; 1.]
 
 (*** define-output:pie1 ***)
 [
---
+++ docs/content/line-scatter-plots.fsx
@@ -0,0 +1,25 @@
+(*** hide ***)
+#r "../../bin/Newtonsoft.Json.dll"
+#r "../../bin/FSharp.Plotly.dll"
+
+(** 
+# FSharp.Plotly: Line and Scatter Charts
+
+*Summary:* This example shows how to create line and point charts in F#.
+
+A line or a point chart can be created using the `Chart.Line` and `Chart.Point` methods. 
+
+
+## Chart.Line with LineStyle
+The following example generates Line Plot containing X and Y values. 
+*)
+
+open FSharp.Plotly 
+  
+let x  = [1.; 2.; 3.; 4.; 5.; 6.; 7.; 8.; 9.; 10.; ]
+let y' = [2.; 1.5; 5.; 1.5; 3.; 2.5; 2.5; 1.5; 3.5; 1.]
+  
+(*** define-output:line1 ***)
+Chart.Line(x,y',Name="line")    
+|> Chart.withLineStyle(Width=2,Dash=StyleOption.DrawingStyle.Dot);
+(*** include-it:line1 ***)
