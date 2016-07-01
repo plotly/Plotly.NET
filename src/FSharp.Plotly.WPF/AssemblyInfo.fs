4a8c70fe7a75378d9d954ed1f6d5cd6bdaeaf0fb
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2016-07-01 19:04:09 +0200

    Bump version to 0.6.1-beta

--- src/FSharp.Plotly.WPF/AssemblyInfo.fs
+++ src/FSharp.Plotly.WPF/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Plotly.WPF")>]
 [<assembly: AssemblyProductAttribute("FSharp.Plotly")>]
 [<assembly: AssemblyDescriptionAttribute("A F# interactive charting library using plotly.js")>]
-[<assembly: AssemblyVersionAttribute("0.6.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.6.0")>]
+[<assembly: AssemblyVersionAttribute("0.6.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.6.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.6.0"
-    let [<Literal>] InformationalVersion = "0.6.0"
+    let [<Literal>] Version = "0.6.1"
+    let [<Literal>] InformationalVersion = "0.6.1"
