370b4403e16c13997fb369028a8fe652be196e07
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2018-03-01 10:36:22 +0100

    Move project towards .netstadard 2.0

---
+++ src/_FSharp.Plotly.WPF/ChartWPF.fs
@@ -0,0 +1,23 @@
+﻿namespace FSharp.Plotly.WPF
+
+
+[<AutoOpen>]
+module ChartWPF =
+
+
+    open System
+    open System.IO
+
+    open FSharp.Plotly
+    open GenericChart
+
+
+
+    type Chart with  
+
+
+        static member ShowWPF (ch:GenericChart) = 
+            let guid = Guid.NewGuid().ToString()
+            let html = GenericChart.toEmbeddedHTML ch
+            ViewContainer.showHTML html
+
