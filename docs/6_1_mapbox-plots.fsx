2875e36119b507d8357e9d2bcbcde9ee51a76323
Kevin Schneider <schneider.kev@outlook.de>
2021-07-06 17:21:57 +0200

    Add PointMapbox and LineMapbox docs

--- docs/6_1_mapbox-plots.fsx
+++ docs/6_1_mapbox-plots.fsx
@@ -28,5 +28,98 @@ index: 2
 
 *Summary:* This example shows how to create Point and Line charts on Mapbox maps in F#.
 
-*More coming soon<sup>TM</sup>*
+let's first create some data for the purpose of creating example charts:
 *)
+open Plotly.NET 
+
+let cityNames = [
+    "Montreal"; "Toronto"; "Vancouver"; "Calgary"; "Edmonton";
+    "Ottawa"; "Halifax"; "Victoria"; "Winnepeg"; "Regina"
+]
+
+let lon = [
+    -73.57; -79.24; -123.06; -114.1; -113.28;
+    -75.43; -63.57; -123.21; -97.13; -104.6
+]
+let lat = [
+    45.5; 43.4; 49.13; 51.1; 53.34; 45.24;
+    44.64; 48.25; 49.89; 50.45
+]
+
+(**
+The simplest type of geographic plot using Mapbox is plotting the (lon,lat) pairs of a location via `Chart.PointMapbox`. 
+Here is an example using the location of Canadian cities:
+*)
+
+let pointMapbox = 
+    Chart.PointMapbox(
+        lon,lat,
+        Labels = cityNames,
+        TextPosition = StyleParam.TextPosition.TopCenter
+    )
+    |> Chart.withMapbox(
+        Mapbox.init(
+            Style=StyleParam.MapboxStyle.OpenStreetMap,
+            Center=(-104.6,50.45)
+        )
+    )
+
+(*** condition: ipynb ***)
+#if IPYNB
+pointMapbox
+#endif // IPYNB
+
+(***hide***)
+pointMapbox |> GenericChart.toChartHTML
+(***include-it-raw***)
+
+(**
+To connect the given (lon,lat) pairs via straight lines, use `Chart.LineGeo`. 
+Below is an example that pulls external data as a Deedle data 
+frame containing the source and target locations of American Airlines flights from Feb. 2011:
+*)
+
+#r "nuget: Deedle"
+#r "nuget: FSharp.Data"
+open Deedle
+open FSharp.Data
+open System.IO
+open System.Text
+
+let data = 
+    Http.RequestString "https://raw.githubusercontent.com/plotly/datasets/c34aaa0b1b3cddad335173cb7bc0181897201ee6/2011_february_aa_flight_paths.csv"
+    |> fun csv -> Frame.ReadCsvString(csv,true,separators=",")
+
+let opacityVals : float [] = data.["cnt"] |> Series.values |> fun s -> s |> Seq.map (fun v -> v/(Seq.max s)) |> Array.ofSeq
+let startCoords = Series.zipInner data.["start_lon"] data.["start_lat"]
+let endCoords = Series.zipInner data.["end_lon"] data.["end_lat"]
+let coords = Series.zipInner startCoords endCoords |> Series.values
+
+let flights = 
+    coords 
+    |> Seq.mapi (fun i (startCoords,endCoords) ->
+        Chart.LineMapbox(
+            [startCoords; endCoords],
+            Opacity = opacityVals.[i],
+            Color = "red"
+        )
+    )
+    |> Chart.Combine
+    |> Chart.withLegend(false)
+    |> Chart.withMapbox(
+        Mapbox.init(
+            Style=StyleParam.MapboxStyle.OpenStreetMap,
+            Center=(-97.0372,32.8959)
+        )
+    )
+    |> Chart.withMarginSize(0,0,50,0)
+    |> Chart.withTitle "Feb. 2011 American Airline flights"
+
+(*** condition: ipynb ***)
+#if IPYNB
+flights
+#endif // IPYNB
+
+(***hide***)
+flights |> GenericChart.toChartHTML
+(***include-it-raw***)
