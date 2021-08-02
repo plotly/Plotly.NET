1642c89fddcac52227f78232e152fa09e33067b8
Kevin Schneider <schneider.kev@outlook.de>
2021-08-02 22:22:11 +0200

    Fix interactive packaging location

--- src/Plotly.NET.Interactive/Repack.ps1
+++ src/Plotly.NET.Interactive/Repack.ps1
@@ -3,7 +3,7 @@ Remove-Item -Recurse ~\.nuget\packages\Plotly.NET.Interactive* -Force
 Remove-Item -Recurse ~\.nuget\packages\Plotly.NET* -Force
 
 # build and pack Plotly.NET.Interactive
-dotnet restore
-dotnet clean
-dotnet build -c Release
-dotnet pack -c Release
+cd ../../
+dotnet tool restore
+dotnet fake build
+dotnet pack -c Release -p:PackageVersion=0.0.0-dev -o "./pkg"
