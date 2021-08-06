5f73f65d67a845ea711c76ad443bf14d74eb93e2
WhiteBlackGoose <wbg@angouri.org>
2021-08-06 12:29:14 +0300

    Mapbox charts tests

---
+++ tests/Plotly.NET.Tests/HtmlCodegen/MapboxMapCharts.fs
@@ -0,0 +1,28 @@
+﻿module Tests.MapboxMapCharts
+
+open Expecto
+open Plotly.NET
+open TestUtils
+open Plotly.NET.GenericChart
+open System
+
+let baseLayerOnlyChart = 
+    let mb =
+        Mapbox.init(
+            Style = StyleParam.MapboxStyle.OpenStreetMap
+        )
+    Chart.PointMapbox([],[]) // deliberately empty chart to show the base map only
+    |> Chart.withMapbox mb // add the mapBox
+
+[<Tests>]
+let `` charts`` =
+    testList "MapboxMapCharts. charts" [
+        testCase "Base layour only data" ( fun () ->
+            "var data = [{\"type\":\"scattermapbox\",\"mode\":\"markers\",\"lon\":[],\"lat\":[],\"line\":{},\"marker\":{}}];"
+            |> chartGeneratedContains baseLayerOnlyChart
+        );
+        testCase "Base layour only layout" ( fun () ->
+            "var layout = {\"mapbox\":{\"style\":\"open-street-map\"}};"
+            |> chartGeneratedContains baseLayerOnlyChart
+        );
+    ]
