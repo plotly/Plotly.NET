e11bdb50e24314da6653c88e9746a9f18ee0d071
WhiteBlackGoose <wbg@angouri.org>
2021-08-06 21:17:29 +0300

    Sankey charts tests

--- src/Plotly.NET/Playground.fsx
+++ src/Plotly.NET/Playground.fsx
@@ -802,18 +802,3 @@ let values,labels =
     |> Seq.unzip
 
 let cols =[|"black";"blue"|]
-
-let r    = [77.5; 72.5; 70.0; 45.0; 22.5; 42.5; 40.0; 62.5]
-let r'   = [57.5; 50.0; 45.0; 35.0; 20.0; 22.5; 37.5; 55.0]
-let r''  = [40.0; 30.0; 30.0; 35.0; 7.5; 7.5; 32.5; 40.0]
-let r''' = [20.0; 7.5; 15.0; 22.5; 2.5; 2.5; 12.5; 22.5]
-
-let t = ["North"; "N-E"; "East"; "S-E"; "South"; "S-W"; "West"; "N-W"]
-[
-    Chart.WindRose (r   ,t,Name="11-14 m/s")
-    Chart.WindRose (r'  ,t,Name="8-11 m/s")
-    Chart.WindRose (r'' ,t,Name="5-8 m/s")
-    Chart.WindRose (r''',t,Name="< 5 m/s")
-]
-|> Chart.Combine
-|> Chart.Show
