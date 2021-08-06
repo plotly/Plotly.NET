bd458cb0bc3767a1365b890e912a1773f175334e
WhiteBlackGoose <wbg@angouri.org>
2021-08-06 21:01:49 +0300

    Polar charts tests

--- src/Plotly.NET/Playground.fsx
+++ src/Plotly.NET/Playground.fsx
@@ -803,8 +803,15 @@ let values,labels =
 
 let cols =[|"black";"blue"|]
 
-let values = [|5; 4; 3; 2; 1|]
-let text = [|"The 1st"; "The 2nd"; "The 3rd"; "The 4th"; "The 5th"|]
-let line = Line.init (Color="purple", Width=3.)
-Chart.FunnelArea(Values=values, Text=text, Line=line)
+let r  = [ 1; 2; 3; 4; 5; 6; 7;]
+let r2 = [ 5; 6; 7; 1; 2; 3; 4;]
+let r3 = [ 3; 1; 5; 2; 8; 7; 5;]
+
+let t  = [0; 45; 90; 135; 200; 320; 184;]
+[
+    Chart.Polar(r,t,StyleParam.Mode.Markers,Name="1")
+    Chart.Polar(r2,t,StyleParam.Mode.Markers,Name="2")
+    Chart.Polar(r3,t,StyleParam.Mode.Markers,Name="3")
+]
+|> Chart.Combine
 |> Chart.Show
---
+++ tests/Plotly.NET.Tests/HtmlCodegen/PolarCharts.fs
@@ -0,0 +1,32 @@
+﻿module Tests.PolarCharts
+
+open Expecto
+open Plotly.NET
+open TestUtils
+open Plotly.NET.GenericChart
+open System
+
+let polar1Chart =
+    let r  = [ 1; 2; 3; 4; 5; 6; 7;]
+    let r2 = [ 5; 6; 7; 1; 2; 3; 4;]
+    let r3 = [ 3; 1; 5; 2; 8; 7; 5;]
+    
+    let t  = [0; 45; 90; 135; 200; 320; 184;]
+    [
+        Chart.Polar(r,t,StyleParam.Mode.Markers,Name="1")
+        Chart.Polar(r2,t,StyleParam.Mode.Markers,Name="2")
+        Chart.Polar(r3,t,StyleParam.Mode.Markers,Name="3")
+    ]
+    |> Chart.Combine
+
+[<Tests>]
+let ``Polar charts`` =
+    testList "PolarCharts.Polar charts" [
+        testCase " data" ( fun () ->
+            "var data = [{\"type\":\"scatter\",\"mode\":\"markers\",\"r\":[1,2,3,4,5,6,7],\"t\":[0,45,90,135,200,320,184],\"name\":\"1\",\"line\":{},\"marker\":{}},{\"type\":\"scatter\",\"mode\":\"markers\",\"r\":[5,6,7,1,2,3,4],\"t\":[0,45,90,135,200,320,184],\"name\":\"2\",\"line\":{},\"marker\":{}},{\"type\":\"scatter\",\"mode\":\"markers\",\"r\":[3,1,5,2,8,7,5],\"t\":[0,45,90,135,200,320,184],\"name\":\"3\",\"line\":{},\"marker\":{}}];"
+            |> chartGeneratedContains polar1Chart
+        );
+        testCase " layout" ( fun () ->
+            emptyLayout polar1Chart
+        );
+    ]
