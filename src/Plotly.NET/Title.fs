074b6e0d1a7f3b26d0d923903f6b7429a3eeca46
Kevin Schneider <schneider.kev@outlook.de>
2021-08-08 14:48:42 +0200

    Add Title object

---
+++ src/Plotly.NET/Title.fs
@@ -0,0 +1,29 @@
+﻿namespace Plotly.NET
+
+type Title() =
+    inherit DynamicObj ()
+
+    static member init
+        (    
+            ?Text:  string,
+            ?Font:  Font
+        ) =    
+            Title()
+            |> Title.style
+                (
+                    ?Text   = Text,
+                    ?Font   = Font
+                )
+
+    static member style
+        (    
+            ?Text:  string,
+            ?Font:  Font
+        ) =
+            (fun (title:Title) -> 
+
+                Text    |> DynObj.setValueOpt title "text"
+                Font    |> DynObj.setValueOpt title "font"
+
+                title
+            )
