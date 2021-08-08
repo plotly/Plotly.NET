c2d8390a190d7993f60cded866fbcf4d45269a9c
Kevin Schneider <schneider.kev@outlook.de>
2021-08-08 15:19:20 +0200

    Add TickFormatStop object

---
+++ src/Plotly.NET/TickFormatStop.fs
@@ -0,0 +1,42 @@
+﻿namespace Plotly.NET
+
+type TickFormatStop() =
+    inherit DynamicObj ()
+
+    static member init
+        (    
+            ?Enabled            : bool,
+            ?DTickRange         : seq<string*string>,
+            ?Value              : string,
+            ?Name               : string,
+            ?TemplateItemName   : string
+        ) =    
+            TickFormatStop()
+            |> TickFormatStop.style
+                (
+                    ?Enabled          = Enabled         ,
+                    ?DTickRange       = DTickRange      ,
+                    ?Value            = Value           ,
+                    ?Name             = Name            ,
+                    ?TemplateItemName = TemplateItemName
+                )
+
+    static member style
+        (    
+            ?Enabled            : bool,
+            ?DTickRange         : seq<string*string>,
+            ?Value              : string,
+            ?Name               : string,
+            ?TemplateItemName   : string
+        ) =
+            (fun (tickFormatStop:TickFormatStop) -> 
+
+                Enabled             |> DynObj.setValueOpt tickFormatStop "enabled"
+                DTickRange          |> DynObj.setValueOpt tickFormatStop "dtickrange"
+                Value               |> DynObj.setValueOpt tickFormatStop "value"
+                Name                |> DynObj.setValueOpt tickFormatStop "name"
+                TemplateItemName    |> DynObj.setValueOpt tickFormatStop "templateitemname"
+
+
+                tickFormatStop
+            )
