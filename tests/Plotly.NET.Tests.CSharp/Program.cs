1b4c6a73d8f8507407088d7f44811e8178d61e56
Kevin Schneider <schneider.kev@outlook.de>
2021-08-24 11:41:40 +0200

    Add C# and F# console projects for local tests

---
+++ tests/Plotly.NET.Tests.CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Plotly.NET;
+
+namespace Plotly.NET.Tests.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
