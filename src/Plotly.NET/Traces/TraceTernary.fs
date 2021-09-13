fba431efb872cf6ebab22a8fd7b4e9a524988b9b
Kevin Schneider <schneider.kev@outlook.de>
2021-09-13 14:24:32 +0200

    Add ScatterTernary TraceStyke

--- src/Plotly.NET/Traces/TraceTernary.fs
+++ src/Plotly.NET/Traces/TraceTernary.fs
@@ -26,3 +26,80 @@ type TraceTernaryStyle() =
 
                 trace
             )
+
+    static member ScatterTernary
+        (
+            [<Optional;DefaultParameterValue(null)>] ?Name              : string,
+            [<Optional;DefaultParameterValue(null)>] ?Visible           : StyleParam.Visible,
+            [<Optional;DefaultParameterValue(null)>] ?ShowLegend        : bool,
+            [<Optional;DefaultParameterValue(null)>] ?LegendRank        : int,
+            [<Optional;DefaultParameterValue(null)>] ?LegendGroup       : string,
+            [<Optional;DefaultParameterValue(null)>] ?LegendGroupTitle  : Title,
+            [<Optional;DefaultParameterValue(null)>] ?Opacity           : float,
+            [<Optional;DefaultParameterValue(null)>] ?Mode              : StyleParam.Mode,
+            [<Optional;DefaultParameterValue(null)>] ?Ids               : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?A                 : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?B                 : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?C                 : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?Text              : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?TextPosition      : StyleParam.TextPosition,
+            [<Optional;DefaultParameterValue(null)>] ?TextTemplate      : string,
+            [<Optional;DefaultParameterValue(null)>] ?HoverText         : string,
+            [<Optional;DefaultParameterValue(null)>] ?HoverInfo         : StyleParam.HoverInfo,
+            [<Optional;DefaultParameterValue(null)>] ?HoverTemplate     : string,
+            [<Optional;DefaultParameterValue(null)>] ?Meta              : string,
+            [<Optional;DefaultParameterValue(null)>] ?CustomData        : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?SubPlot           : StyleParam.SubPlotId,
+            [<Optional;DefaultParameterValue(null)>] ?Marker            : Marker,
+            [<Optional;DefaultParameterValue(null)>] ?Line              : Line,
+            [<Optional;DefaultParameterValue(null)>] ?TextFont          : Font,
+            [<Optional;DefaultParameterValue(null)>] ?SelectedPoints    : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?Selected          : Selection,
+            [<Optional;DefaultParameterValue(null)>] ?Unselected        : Selection,
+            [<Optional;DefaultParameterValue(null)>] ?ClipOnAxis        : bool,
+            [<Optional;DefaultParameterValue(null)>] ?ConnectGaps       : bool,
+            [<Optional;DefaultParameterValue(null)>] ?Fill              : StyleParam.Fill,
+            [<Optional;DefaultParameterValue(null)>] ?FillColor         : Color,
+            [<Optional;DefaultParameterValue(null)>] ?HoverLabel        : Hoverlabel,
+            [<Optional;DefaultParameterValue(null)>] ?HoverOn           : StyleParam.HoverOn,
+            [<Optional;DefaultParameterValue(null)>] ?Sum               : #IConvertible
+
+        ) =
+            fun (trace: TraceTernary) ->
+                
+                Name              |> DynObj.setValueOpt trace "name"
+                Visible           |> DynObj.setValueOptBy trace "visible" StyleParam.Visible.convert
+                ShowLegend        |> DynObj.setValueOpt trace "showlegend"
+                LegendRank        |> DynObj.setValueOpt trace "legendrank"
+                LegendGroup       |> DynObj.setValueOpt trace "legendgroup"
+                LegendGroupTitle  |> DynObj.setValueOpt trace "legendgrouptitle"
+                Opacity           |> DynObj.setValueOpt trace "opacity"
+                Mode              |> DynObj.setValueOptBy trace "mode" StyleParam.Mode.convert
+                Ids               |> DynObj.setValueOpt trace "ids"
+                A                 |> DynObj.setValueOpt trace "a"
+                B                 |> DynObj.setValueOpt trace "b"
+                C                 |> DynObj.setValueOpt trace "c"
+                Text              |> DynObj.setValueOpt trace "text"
+                TextPosition      |> DynObj.setValueOptBy trace "textposition" StyleParam.TextPosition.convert
+                TextTemplate      |> DynObj.setValueOpt trace "texttemplate"
+                HoverText         |> DynObj.setValueOpt trace "hovertext"
+                HoverInfo         |> DynObj.setValueOptBy trace "hoverinfo" StyleParam.HoverInfo.convert
+                HoverTemplate     |> DynObj.setValueOpt trace "hovertemplate"
+                Meta              |> DynObj.setValueOpt trace "meta"
+                CustomData        |> DynObj.setValueOpt trace "customdata"
+                SubPlot           |> DynObj.setValueOptBy trace "subplot" StyleParam.SubPlotId.convert
+                Marker            |> DynObj.setValueOpt trace "marker"
+                Line              |> DynObj.setValueOpt trace "line"
+                TextFont          |> DynObj.setValueOpt trace "textfont"
+                SelectedPoints    |> DynObj.setValueOpt trace "selectedpoints"
+                Selected          |> DynObj.setValueOpt trace "selected"
+                Unselected        |> DynObj.setValueOpt trace "unselected"
+                ClipOnAxis        |> DynObj.setValueOpt trace "cliponaxis"
+                ConnectGaps       |> DynObj.setValueOpt trace "connectgaps"
+                Fill              |> DynObj.setValueOptBy trace "fill" StyleParam.Fill.convert
+                FillColor         |> DynObj.setValueOpt trace "fillcolor"
+                HoverLabel        |> DynObj.setValueOpt trace "hoverlabel"
+                HoverOn           |> DynObj.setValueOptBy trace "hoveron" StyleParam.HoverOn.convert
+                Sum               |> DynObj.setValueOpt trace "sum"
+
+                trace
