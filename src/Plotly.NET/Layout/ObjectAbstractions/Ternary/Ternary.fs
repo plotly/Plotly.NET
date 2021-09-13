6512e2e79c8893935eff884547bfc3e41c279281
Kevin Schneider <schneider.kev@outlook.de>
2021-09-13 14:37:53 +0200

    Add Ternary object

---
+++ src/Plotly.NET/Layout/ObjectAbstractions/Ternary/Ternary.fs
@@ -0,0 +1,62 @@
+﻿namespace Plotly.NET.LayoutObjects
+
+open Plotly.NET
+open DynamicObj
+open System
+open System.Runtime.InteropServices
+
+type Ternary() =
+    inherit DynamicObj ()
+
+    /// <summary>
+    /// Initializes a ternary object
+    /// </summary>
+    /// <param name="AAxis">Sets the ternary A Axis</param>
+    /// <param name="BAxis">Sets the ternary B Axis</param>
+    /// <param name="CAxis">Sets the ternary C Axis</param>
+    /// <param name="Domain">Sets the ternary domain</param>
+    /// <param name="Sum">The number each triplet should sum to, and the maximum range of each axis</param>
+    static member init
+        (    
+            ?AAxis      : LinearAxis,
+            ?BAxis      : LinearAxis,
+            ?CAxis      : LinearAxis,
+            ?Domain     : Domain,
+            ?Sum        : #IConvertible
+        ) =    
+            Ternary()
+            |> Ternary.style
+                (
+                    ?AAxis  = AAxis ,
+                    ?BAxis  = BAxis ,
+                    ?CAxis  = CAxis ,
+                    ?Domain = Domain,
+                    ?Sum    = Sum   
+                )
+
+    /// <summary>
+    /// Creates a function that applies the given style parameters to a Ternary object.
+    /// </summary>
+    /// <param name="AAxis">Sets the ternary A Axis</param>
+    /// <param name="BAxis">Sets the ternary B Axis</param>
+    /// <param name="CAxis">Sets the ternary C Axis</param>
+    /// <param name="Domain">Sets the ternary domain</param>
+    /// <param name="Sum">The number each triplet should sum to, and the maximum range of each axis</param>
+    static member style
+        (    
+            ?AAxis      : LinearAxis,
+            ?BAxis      : LinearAxis,
+            ?CAxis      : LinearAxis,
+            ?Domain     : Domain,
+            ?Sum        : #IConvertible
+        ) =
+            (fun (ternary:Ternary) -> 
+               
+                AAxis  |> DynObj.setValueOpt ternary "aaxis"
+                BAxis  |> DynObj.setValueOpt ternary "baxis"
+                CAxis  |> DynObj.setValueOpt ternary "caxis"
+                Domain |> DynObj.setValueOpt ternary "domain"
+                Sum    |> DynObj.setValueOpt ternary "sum"
+
+                ternary
+            )
