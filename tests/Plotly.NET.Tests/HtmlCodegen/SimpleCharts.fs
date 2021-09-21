f226e9554c466a09a33c59f761b6d67db49e30a0
Kevin Schneider <schneider.kev@outlook.de>
2021-09-21 15:24:05 +0200

    Fix marker test

--- tests/Plotly.NET.Tests/HtmlCodegen/SimpleCharts.fs
+++ tests/Plotly.NET.Tests/HtmlCodegen/SimpleCharts.fs
@@ -45,7 +45,7 @@ let textLabelChart =
 let ``Line and scatter plots`` =
     testList "SimpleCharts.Line and scatter plots" [
         testCase "With LineStyle data" ( fun () ->
-            """var data = [{"type":"scatter","mode":"lines+markers","x":[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0],"y":[2.0,1.5,5.0,1.5,3.0,2.5,2.5,1.5,3.5,1.0],"line":{"width":2.0,"dash":"dot"},"name":"line","marker":{"symbol":1}}];"""
+            """var data = [{"type":"scatter","mode":"lines+markers","x":[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0],"y":[2.0,1.5,5.0,1.5,3.0,2.5,2.5,1.5,3.5,1.0],"line":{"width":2.0,"dash":"dot"},"name":"line","marker":{"symbol":"1"}}];"""
             |> chartGeneratedContains withLineStyleChart
         );
         testCase "With LineStyle layout" ( fun () ->
