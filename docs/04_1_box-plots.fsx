9d915cbfc9797f79c6908710f042f86d009c686d
Kevin Schneider <schneider.kev@outlook.de>
2021-10-13 10:08:08 +0200

    fix box/violin docs

--- docs/04_1_box-plots.fsx
+++ docs/04_1_box-plots.fsx
@@ -45,7 +45,7 @@ Outliers may be plotted as individual points.
 *)
 
 let box1 =
-    Chart.BoxPlot(x,y,Jitter=0.1,Boxpoints=StyleParam.Boxpoints.All)
+    Chart.BoxPlot(x,y,Jitter=0.1,BoxPoints=StyleParam.BoxPoints.All)
 
 (*** condition: ipynb ***)
 #if IPYNB
@@ -60,7 +60,7 @@ box1 |> GenericChart.toChartHTML
 By swapping x and y plus using `StyleParam.Orientation.Horizontal` we can flip the chart horizontaly.
 *)
 let box2 =
-    Chart.BoxPlot(y,x,Jitter=0.1,Boxpoints=StyleParam.Boxpoints.All,Orientation=StyleParam.Orientation.Horizontal)
+    Chart.BoxPlot(y,x,Jitter=0.1,BoxPoints=StyleParam.BoxPoints.All,Orientation=StyleParam.Orientation.Horizontal)
 
 (*** condition: ipynb ***)
 #if IPYNB
@@ -79,8 +79,8 @@ let y' =  [2.; 1.5; 5.; 1.5; 2.; 2.5; 2.1; 2.5; 1.5; 1.;2.; 1.5; 5.; 1.5; 3.; 2.
 
 let box3 =
     [
-        Chart.BoxPlot("y" ,y,Name="bin1",Jitter=0.1,Boxpoints=StyleParam.Boxpoints.All);
-        Chart.BoxPlot("y'",y',Name="bin2",Jitter=0.1,Boxpoints=StyleParam.Boxpoints.All);
+        Chart.BoxPlot("y" ,y,Name="bin1",Jitter=0.1,BoxPoints=StyleParam.BoxPoints.All);
+        Chart.BoxPlot("y'",y',Name="bin2",Jitter=0.1,BoxPoints=StyleParam.BoxPoints.All);
     ]
     |> Chart.combine
 
