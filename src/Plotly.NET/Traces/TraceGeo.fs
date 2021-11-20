c84a9e8171de2734a22840ae5241cdaa07be5697
Kevin Schneider <schneider.kev@outlook.de>
2021-11-20 11:20:29 +0100

    update scattergeo trace and chart constructors

--- src/Plotly.NET/Traces/TraceGeo.fs
+++ src/Plotly.NET/Traces/TraceGeo.fs
@@ -91,29 +91,84 @@ type TraceGeoStyle() =
 
     static member ScatterGeo 
         (
-            [<Optional;DefaultParameterValue(null)>] ?Mode       : StyleParam.Mode,
-            [<Optional;DefaultParameterValue(null)>] ?Longitudes : #IConvertible seq,
-            [<Optional;DefaultParameterValue(null)>] ?Latitudes  : #IConvertible seq,
-            [<Optional;DefaultParameterValue(null)>] ?Locations  : seq<string>,
-            [<Optional;DefaultParameterValue(null)>] ?GeoJson    ,
-            [<Optional;DefaultParameterValue(null)>] ?FeatureIdKey:string,
-            [<Optional;DefaultParameterValue(null)>] ?Connectgaps : bool,
-            [<Optional;DefaultParameterValue(null)>] ?Fill        : StyleParam.Fill,
-            [<Optional;DefaultParameterValue(null)>] ?Fillcolor   : Color,
-            [<Optional;DefaultParameterValue(null)>] ?Marker     : Marker
+            [<Optional;DefaultParameterValue(null)>] ?Name              : string,
+            [<Optional;DefaultParameterValue(null)>] ?Visible           : StyleParam.Visible,
+            [<Optional;DefaultParameterValue(null)>] ?ShowLegend        : bool,
+            [<Optional;DefaultParameterValue(null)>] ?LegendRank        : int,
+            [<Optional;DefaultParameterValue(null)>] ?LegendGroup       : string,
+            [<Optional;DefaultParameterValue(null)>] ?LegendGroupTitle  : Title,
+            [<Optional;DefaultParameterValue(null)>] ?Opacity           : float,
+            [<Optional;DefaultParameterValue(null)>] ?Mode              : StyleParam.Mode,         
+            [<Optional;DefaultParameterValue(null)>] ?Ids               : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?Lat               : #IConvertible seq,
+            [<Optional;DefaultParameterValue(null)>] ?GeoJson           : obj,
+            [<Optional;DefaultParameterValue(null)>] ?FeatureIdKey      : string,
+            [<Optional;DefaultParameterValue(null)>] ?Locations         : seq<string>,
+            [<Optional;DefaultParameterValue(null)>] ?Lon               : #IConvertible seq,
+            [<Optional;DefaultParameterValue(null)>] ?Text              : #IConvertible,
+            [<Optional;DefaultParameterValue(null)>] ?MultiText         : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?TextPosition      : StyleParam.TextPosition,
+            [<Optional;DefaultParameterValue(null)>] ?MultiTextPosition : seq<StyleParam.TextPosition>,
+            [<Optional;DefaultParameterValue(null)>] ?TextTemplate      : string,
+            [<Optional;DefaultParameterValue(null)>] ?MultiTextTemplate : seq<string>,
+            [<Optional;DefaultParameterValue(null)>] ?HoverText         : string,
+            [<Optional;DefaultParameterValue(null)>] ?MultiHoverText    : seq<string>,
+            [<Optional;DefaultParameterValue(null)>] ?HoverInfo         : StyleParam.HoverInfo,
+            [<Optional;DefaultParameterValue(null)>] ?HoverTemplate     : string,
+            [<Optional;DefaultParameterValue(null)>] ?MultiHoverTemplate: seq<string>,
+            [<Optional;DefaultParameterValue(null)>] ?Meta              : string,
+            [<Optional;DefaultParameterValue(null)>] ?CustomData        : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?Geo               : StyleParam.SubPlotId,
+            [<Optional;DefaultParameterValue(null)>] ?Marker            : Marker,
+            [<Optional;DefaultParameterValue(null)>] ?Line              : Line,
+            [<Optional;DefaultParameterValue(null)>] ?TextFont          : Font,
+            [<Optional;DefaultParameterValue(null)>] ?SelectedPoints    : seq<#IConvertible>,
+            [<Optional;DefaultParameterValue(null)>] ?Selected          : Selection,
+            [<Optional;DefaultParameterValue(null)>] ?Unselected        : Selection,
+            [<Optional;DefaultParameterValue(null)>] ?ConnectGaps       : bool,
+            [<Optional;DefaultParameterValue(null)>] ?Fill              : StyleParam.Fill,
+            [<Optional;DefaultParameterValue(null)>] ?FillColor         : Color,
+            [<Optional;DefaultParameterValue(null)>] ?HoverLabel        : Hoverlabel,
+            [<Optional;DefaultParameterValue(null)>] ?LocationMode      : StyleParam.LocationFormat,
+            [<Optional;DefaultParameterValue(null)>] ?UIRevision        : string
         ) =
             (fun (trace: #Trace) -> 
-            
-                Mode        |> DynObj.setValueOptBy trace "mode" StyleParam.Mode.convert 
-                Longitudes  |> DynObj.setValueOpt   trace "lon"
-                Latitudes   |> DynObj.setValueOpt   trace "lat"
-                Locations   |> DynObj.setValueOpt   trace "locations"
-                GeoJson     |> DynObj.setValueOpt   trace "geojson"
-                FeatureIdKey|> DynObj.setValueOpt   trace "featureidkey"
-                Connectgaps |> DynObj.setValueOpt   trace "connectgaps"
-                Fill        |> DynObj.setValueOptBy trace "fill" StyleParam.Fill.convert
-                Fillcolor   |> DynObj.setValueOpt   trace "fillcolor"
-                Marker      |> DynObj.setValueOpt   trace "marker"
+                
+                Name                                |> DynObj.setValueOpt trace "name"
+                Visible                             |> DynObj.setValueOptBy trace "visible" StyleParam.Visible.convert
+                ShowLegend                          |> DynObj.setValueOpt trace "showlegend"
+                LegendRank                          |> DynObj.setValueOpt trace "legendrank"
+                LegendGroup                         |> DynObj.setValueOpt trace "legendgroup"
+                LegendGroupTitle                    |> DynObj.setValueOpt trace "legendgrouptitle"
+                Opacity                             |> DynObj.setValueOpt trace "opacity"
+                Mode                                |> DynObj.setValueOptBy trace "mode" StyleParam.Mode.convert
+                Ids                                 |> DynObj.setValueOpt trace "ids"
+                Lat                                 |> DynObj.setValueOpt trace "lat"
+                GeoJson                             |> DynObj.setValueOpt trace "geojson"
+                FeatureIdKey                        |> DynObj.setValueOpt trace "featureidkey"
+                Locations                           |> DynObj.setValueOpt trace "locations"
+                Lon                                 |> DynObj.setValueOpt trace "lon"
+                (Text, MultiText)                   |> DynObj.setSingleOrMultiOpt trace "text"
+                (TextPosition, MultiTextPosition)   |> DynObj.setSingleOrMultiOptBy trace "textposition" StyleParam.TextPosition.convert
+                (TextTemplate, MultiTextTemplate)   |> DynObj.setSingleOrMultiOpt trace "texttemplate"
+                (HoverText, MultiHoverText)         |> DynObj.setSingleOrMultiOpt trace "hovertext"
+                HoverInfo                           |> DynObj.setValueOptBy trace "hoverinfo" StyleParam.HoverInfo.convert
+                (HoverTemplate, MultiHoverTemplate) |> DynObj.setSingleOrMultiOpt trace "hovertemplate"
+                Meta                                |> DynObj.setValueOpt trace "meta"
+                CustomData                          |> DynObj.setValueOpt trace "customdata"
+                Geo                                 |> DynObj.setValueOptBy trace "geo" StyleParam.SubPlotId.convert
+                Marker                              |> DynObj.setValueOpt trace "marker"
+                Line                                |> DynObj.setValueOpt trace "line"
+                TextFont                            |> DynObj.setValueOpt trace "textfont"
+                SelectedPoints                      |> DynObj.setValueOpt trace "selectedpoints"
+                Selected                            |> DynObj.setValueOpt trace "selected"
+                Unselected                          |> DynObj.setValueOpt trace "unselected"
+                ConnectGaps                         |> DynObj.setValueOpt trace "connectgaps"
+                Fill                                |> DynObj.setValueOptBy trace "fill" StyleParam.Fill.convert
+                FillColor                           |> DynObj.setValueOpt trace "fillcolor"
+                HoverLabel                          |> DynObj.setValueOpt trace "hoverlabel"
+                LocationMode                        |> DynObj.setValueOptBy trace "locationmode" StyleParam.LocationFormat.convert
+                UIRevision                          |> DynObj.setValueOpt trace "uirevision"
 
                 trace
 
