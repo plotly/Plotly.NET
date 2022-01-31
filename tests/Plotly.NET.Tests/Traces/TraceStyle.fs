5452be4b86ef0546978dbe05d49f5c27e8df5f4e
Kevin Schneider <schneider.kev@outlook.de>
2022-01-31 15:47:16 +0100

    Add Some TraceStyle tests

---
+++ tests/Plotly.NET.Tests/Traces/TraceStyle.fs
@@ -0,0 +1,38 @@
+﻿module Tests.LayoutObjects.TraceStyle
+
+open Expecto
+open Plotly.NET
+open Plotly.NET.LayoutObjects
+open Plotly.NET.TraceObjects
+open Plotly.NET.GenericChart
+
+let createEmptyTrace() = Trace2D.initScatter(id)
+
+[<Tests>]
+let ``TraceStyle tests`` =
+    testList "Traces.TraceStyle" [
+        testCase "SetMarker" (fun _ ->
+            Expect.equal
+                (createEmptyTrace() |> TraceStyle.SetMarker(Marker.init(Color=Color.fromKeyword Red, Opacity = 0.)))
+                (Trace2D.initScatter(Trace2DStyle.Scatter(Marker = Marker.init(Color=Color.fromKeyword Red, Opacity = 0.))))
+                "TraceStyle.SetMarker did not produce the correct trace object"
+        )
+        testCase "Marker" (fun _ ->
+            Expect.equal
+                (createEmptyTrace() |> TraceStyle.Marker(Color=Color.fromKeyword Red, Opacity = 0.))
+                (Trace2D.initScatter(Trace2DStyle.Scatter(Marker = Marker.init(Color=Color.fromKeyword Red, Opacity = 0.))))
+                "TraceStyle.Marker did not produce the correct trace object"
+        )
+        testCase "SetLine" (fun _ ->
+            Expect.equal
+                (createEmptyTrace() |> TraceStyle.SetLine(Line.init(Color=Color.fromKeyword Red, Width = 0.)))
+                (Trace2D.initScatter(Trace2DStyle.Scatter(Line = Line.init(Color=Color.fromKeyword Red, Width = 0.))))
+                "TraceStyle.SetLine did not produce the correct trace object"
+        )
+        testCase "Line" (fun _ ->
+            Expect.equal
+                (createEmptyTrace() |> TraceStyle.Line(Color=Color.fromKeyword Red, Width = 0.))
+                (Trace2D.initScatter(Trace2DStyle.Scatter(Line = Line.init(Color=Color.fromKeyword Red, Width = 0.))))
+                "TraceStyle.Line did not produce the correct trace object"
+        )
+    ]
