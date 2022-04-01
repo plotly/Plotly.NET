dc7091b73e7928a357984692830bd12a659236d9
Kevin Schneider <schneider.kev@outlook.de>
2022-04-01 20:45:40 +0200

    add verify docs target

--- build/Build.fs
+++ build/Build.fs
@@ -16,6 +16,56 @@ open ReleaseTasks
 
 initializeContext ()
 
+open FSharp.Compiler.Diagnostics
+open BasicTasks
+
+let verifyDocs =
+    BuildTask.create "VerifyDocs" [ clean; build ] {
+        let targets =
+            !! "docs/**.fsx" |> Seq.map (fun f -> f.ToString())
+
+        let checker =
+            FSharp.Compiler.CodeAnalysis.FSharpChecker.Create()
+
+        let ignoredDiagnostics = Set.ofList [ 1182 ] // unused variable
+
+        targets
+        |> Seq.map (fun target ->
+            checker.Compile(
+                [|
+                    "fsc.exe"
+                    "-o"
+                    @"aaaaaaaaaaa.exe"
+                    "-a"
+                    target
+                |]
+            )
+            |> Async.RunSynchronously
+            |> fst
+            |> Seq.where (fun diag ->
+                match diag.Severity with
+                | FSharpDiagnosticSeverity.Error
+                | FSharpDiagnosticSeverity.Warning -> true
+                | _ -> false))
+        |> Seq.collect id
+        |> Seq.where (fun c -> not (ignoredDiagnostics.Contains c.ErrorNumber))
+        |> Seq.sortBy (fun diag ->
+            (match diag.Severity with
+             | FSharpDiagnosticSeverity.Error -> 0
+             | _ -> 1),
+            diag.FileName.[..6 (* to only count the numeric part *) ])
+        |> Seq.map (fun diag ->
+            (match diag.Severity with
+             | FSharpDiagnosticSeverity.Error -> "--- Error: "
+             | _ -> "--- Warning: ")
+            + diag.ToString())
+        |> String.concat "\n"
+        |> (fun errorText ->
+            match errorText with
+            | "" -> ()
+            | text -> raise (System.Exception $"Errors:\n{text}"))
+    }
+
 let sourceFiles =
     !! "src/Plotly.NET/**/*.fs"
     ++ "src/Plotly.NET.ImageExport/**/*.fs"
