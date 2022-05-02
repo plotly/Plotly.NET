e5a0617d860d63a3096ccc779ea55930c9ec9559
Kevin Schneider <schneider.kev@outlook.de>
2022-05-02 13:50:55 +0200

    POC for extension method written in C#

---
+++ src/Plotly.NET.CSharp/GenericChartExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Plotly.NET;
+
+namespace Plotly.NET.CSharp
+{
+    public static class GenericChartExtensions
+    {
+        public static Layout GetLayout(this GenericChart.GenericChart gChart)
+        {
+            return GenericChart.getLayout(gChart);
+        }
+
+        public static Trace [] GetTraces(this GenericChart.GenericChart gChart)
+        {
+            return GenericChart.getTraces(gChart).ToArray();
+        }
+
+        public static GenericChart.GenericChart WithTraceInfo(
+            this GenericChart.GenericChart gChart,
+            string? Name,
+            StyleParam.Visible? Visible,
+            bool? ShowLegend,
+            int? LegendRank,
+            string? LegendGroup,
+            Title? LegendGroupTitle
+        )
+        {
+            return
+                Plotly.NET.Chart.WithTraceInfo(
+                    Name: Name,
+                    Visible: Visible,
+                    ShowLegend: ShowLegend,
+                    LegendRank: LegendRank,
+                    LegendGroup: LegendGroup,
+                    LegendGroupTitle: LegendGroupTitle
+                ).Invoke(gChart);
+        }
+    }
+}
---
+++ tests/Plotly.NET.CSharp.Tests/ExtensionMethodsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+using Plotly.NET.CSharp;
+
+
+namespace Plotly.NET.CSharp.Tests
+{
+    public class ExtensionMethodsTests
+    {
+        internal GenericChart.GenericChart chart = Chart2D.Chart.Point<double, double, string>(x: new double[] { 1, 2 }, y: new double[] { 5, 10 });
+
+        [Fact]
+        public void CanUseCSharpExtensionMethod()
+        {
+            Trace actual =
+                chart
+                .WithTraceInfo(Name: "Name")
+                .GetTraces()
+                [0];
+
+            Assert.Equal("Name", DynamicObj.DynamicObj.GetValue(actual,"name"));
+        }
+    }
+}
