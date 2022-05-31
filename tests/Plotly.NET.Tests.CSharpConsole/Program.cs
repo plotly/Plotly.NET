0a2eafe874c0712a586caca50e51f5f572aeff5b
Kevin Schneider <schneider.kev@outlook.de>
2022-05-31 09:01:29 +0200

    Add C# bindings for Chart.Grid and Chart.Scatter3D

--- tests/Plotly.NET.Tests.CSharpConsole/Program.cs
+++ tests/Plotly.NET.Tests.CSharpConsole/Program.cs
@@ -9,23 +9,32 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            Chart.Combine(
-                new GenericChart []
-                {
-                    Chart.Scatter<int,int,string>(
-                        x: new int [] { 1, 2 },
-                        y: new int [] { 3, 4 },
-                        mode: Mode.Markers
-                    ),
-                    Chart.Point<int,int,string>(
-                        x: new int [] { 5, 6 },
-                        y: new int [] { 7, 8 }
-                    ),
-                    Chart.Line<int,int,string>(
-                        x: new int [] { 9, 10 },
-                        y: new int [] { 11, 12 }
-                    )
-                }
+            Chart.Grid(
+                nRows: 2,
+                nCols: 3,
+                gCharts:
+                    new GenericChart []
+                    {
+                        Chart.Scatter<int,int,string>(
+                            x: new int [] { 1, 2 },
+                            y: new int [] { 3, 4 },
+                            mode: Mode.Markers
+                        ),
+                        Chart.Point<int,int,string>(
+                            x: new int [] { 5, 6 },
+                            y: new int [] { 7, 8 }
+                        ),
+                        Chart.Line<int,int,string>(
+                            x: new int [] { 9, 10 },
+                            y: new int [] { 11, 12 }
+                        ),
+                        Chart.Scatter3D<int,int,int,string>(
+                            x: new int[] { 12, 13 },
+                            y: new int [] { 13, 14 },
+                            z: new int [] { 14, 15 },
+                            mode: Mode.Markers
+                        )
+                    }
             ).Show();
         }
         static void Main2(string[] args)
