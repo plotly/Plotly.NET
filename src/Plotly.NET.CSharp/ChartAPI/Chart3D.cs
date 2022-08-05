796eea391bd9f9dce0ef3769a9a32f31390b43de
Kevin Schneider <schneider.kev@outlook.de>
2022-08-05 14:57:11 +0200

    Add C# bindings for Chart.Mesh3D and Chart.Cone

--- src/Plotly.NET.CSharp/ChartAPI/Chart3D.cs
+++ src/Plotly.NET.CSharp/ChartAPI/Chart3D.cs
@@ -396,5 +396,163 @@ namespace Plotly.NET.CSharp
                     ShowScale: ShowScale.ToOption(),
                     UseDefaults: UseDefaults.ToOption()
                 );
+
+        /// <summary>
+        /// Visualizes a 3D mesh.
+        ///
+        /// Draws sets of triangles with coordinates given by three 1-dimensional arrays in `x`, `y`, `z` and
+        ///
+        /// (1) a sets of `i`, `j`, `k` indices or
+        ///
+        /// (2) Delaunay triangulation or
+        ///
+        /// (3) the Alpha-shape algorithm or
+        ///
+        /// (4) the Convex-hull algorithm
+        /// </summary>
+        /// <param name="x">Sets the X coordinates of the vertices. The nth element of vectors `x`, `y` and `z` jointly represent the X, Y and Z coordinates of the nth vertex.</param>
+        /// <param name="y">Sets the Y coordinates of the vertices. The nth element of vectors `x`, `y` and `z` jointly represent the X, Y and Z coordinates of the nth vertex.</param>
+        /// <param name="z">Sets the Z coordinates of the vertices. The nth element of vectors `x`, `y` and `z` jointly represent the X, Y and Z coordinates of the nth vertex.</param>
+        /// <param name="I">A vector of vertex indices, i.e. integer values between 0 and the length of the vertex vectors, representing the "first" vertex of a triangle. For example, `{i[m], j[m], k[m]}` together represent face m (triangle m) in the mesh, where `i[m] = n` points to the triplet `{x[n], y[n], z[n]}` in the vertex arrays. Therefore, each element in `i` represents a point in space, which is the first vertex of a triangle.</param>
+        /// <param name="J">A vector of vertex indices, i.e. integer values between 0 and the length of the vertex vectors, representing the "second" vertex of a triangle. For example, `{i[m], j[m], k[m]}` together represent face m (triangle m) in the mesh, where `j[m] = n` points to the triplet `{x[n], y[n], z[n]}` in the vertex arrays. Therefore, each element in `j` represents a point in space, which is the second vertex of a triangle.</param>
+        /// <param name="K">A vector of vertex indices, i.e. integer values between 0 and the length of the vertex vectors, representing the "third" vertex of a triangle. For example, `{i[m], j[m], k[m]}` together represent face m (triangle m) in the mesh, where `k[m] = n` points to the triplet `{x[n], y[n], z[n]}` in the vertex arrays. Therefore, each element in `k` represents a point in space, which is the third vertex of a triangle.</param>
+        /// <param name="Name">Sets the trace name. The trace name appear as the legend item and on hover</param>
+        /// <param name="ShowLegend">Determines whether or not an item corresponding to this trace is shown in the legend.</param>
+        /// <param name="Opacity">Sets the opactity of the trace</param>
+        /// <param name="Text">Sets a text associated with each datum</param>
+        /// <param name="MultiText">Sets individual text for each datum</param>
+        /// <param name="Color">Sets the color of the whole mesh</param>
+        /// <param name="Contour">Sets the style and visibility of contours</param>
+        /// <param name="ColorScale">Sets the colorscale</param>
+        /// <param name="ShowScale">Wether or not to show the colorbar/colorscale</param>
+        /// <param name="ColorBar">Sets the colorbar</param>
+        /// <param name="FlatShading">Determines whether or not normal smoothing is applied to the meshes, creating meshes with an angular, low-poly look via flat reflections.</param>
+        /// <param name="TriangulationAlgorithm">Determines how the mesh surface triangles are derived from the set of vertices (points) represented by the `x`, `y` and `z` arrays, if the `i`, `j`, `k` arrays are not supplied.</param>
+        /// <param name="UseDefaults">If set to false, ignore the global default settings set in `Defaults`</param>
+        public static GenericChart.GenericChart Mesh3D<XType, YType, ZType, IType, JType, KType, TextType>(
+            IEnumerable<XType> x, 
+            IEnumerable<YType> y, 
+            IEnumerable<ZType> z, 
+            Optional<IEnumerable<IType>> I = default, 
+            Optional<IEnumerable<JType>> J = default, 
+            Optional<IEnumerable<KType>> K = default, 
+            Optional<string> Name = default, 
+            Optional<bool> ShowLegend = default, 
+            Optional<double> Opacity = default, 
+            Optional<TextType> Text = default, 
+            Optional<IEnumerable<TextType>> MultiText = default, 
+            Optional<Color> Color = default, 
+            Optional<Contour> Contour = default, 
+            Optional<StyleParam.Colorscale> ColorScale = default, 
+            Optional<bool> ShowScale = default, 
+            Optional<ColorBar> ColorBar = default, 
+            Optional<bool> FlatShading = default,
+            Optional<StyleParam.TriangulationAlgorithm> TriangulationAlgorithm = default, 
+            Optional<bool> UseDefaults = default
+        )
+            where XType : IConvertible
+            where YType : IConvertible
+            where ZType : IConvertible
+            where IType : IConvertible
+            where JType : IConvertible
+            where KType : IConvertible
+            where TextType : IConvertible
+            =>
+                Plotly.NET.Chart3D.Chart.Mesh3D<XType, YType, ZType, IType, JType, KType, TextType>(
+                    x: x,
+                    y: y,
+                    z: z,
+                    I: I.ToOption(),
+                    J: J.ToOption(),
+                    K: K.ToOption(),
+                    Name: Name.ToOption(),
+                    ShowLegend: ShowLegend.ToOption(),
+                    Opacity: Opacity.ToOption(),
+                    Text: Text.ToOption(),
+                    MultiText: MultiText.ToOption(),
+                    Color: Color.ToOption(),
+                    Contour: Contour.ToOption(),
+                    ColorScale: ColorScale.ToOption(),
+                    ShowScale: ShowScale.ToOption(),
+                    ColorBar: ColorBar.ToOption(),
+                    FlatShading: FlatShading.ToOption(),
+                    TriangulationAlgorithm: TriangulationAlgorithm.ToOption(),
+                    UseDefaults: UseDefaults.ToOption()
+                );
+
+        /// <summary>
+        /// Creates a cone plot, typically used to visualize vector fields.
+        ///
+        /// Specify a vector field using 6 1D arrays:
+        ///
+        /// 3 position arrays `x`, `y` and `z` and
+        ///
+        /// 3 vector component arrays `u`, `v`, `w`.
+        ///
+        /// The cones are drawn exactly at the positions given by `x`, `y` and `z`.
+        /// </summary>
+        /// <param name="x">Sets the x coordinates of the vector field and of the displayed cones.</param>
+        /// <param name="y">Sets the y coordinates of the vector field and of the displayed cones.</param>
+        /// <param name="z">Sets the z coordinates of the vector field and of the displayed cones.</param>
+        /// <param name="u">Sets the x components of the vector field.</param>
+        /// <param name="v">Sets the y components of the vector field.</param>
+        /// <param name="w">Sets the z components of the vector field.</param>
+        /// <param name="Name">Sets the trace name. The trace name appear as the legend item and on hover</param>
+        /// <param name="ShowLegend">Determines whether or not an item corresponding to this trace is shown in the legend.</param>
+        /// <param name="Opacity">Sets the opactity of the trace</param>
+        /// <param name="Text">Sets a text associated with each datum</param>
+        /// <param name="MultiText">Sets individual text for each datum</param>
+        /// <param name="ColorScale">Sets the colorscale</param>
+        /// <param name="ShowScale">Wether or not to show the colorbar/colorscale</param>
+        /// <param name="ColorBar">Sets the colorbar</param>
+        /// <param name="SizeMode">Determines whether `sizeref` is set as a "scaled" (i.e unitless) scalar (normalized by the max u/v/w norm in the vector field) or as "absolute" value (in the same units as the vector field).</param>
+        /// <param name="ConeAnchor">Sets the cones' anchor with respect to their x/y/z positions. Note that "cm" denote the cone's center of mass which corresponds to 1/4 from the tail to tip.</param>
+        /// <param name="UseDefaults">If set to false, ignore the global default settings set in `Defaults`</param>
+        public static GenericChart.GenericChart Cone<XType, YType, ZType, UType, VType, WType, TextType>(
+            IEnumerable<XType> x, 
+            IEnumerable<YType> y, 
+            IEnumerable<ZType> z, 
+            IEnumerable<UType> u, 
+            IEnumerable<VType> v, 
+            IEnumerable<WType> w, 
+            Optional<string> Name = default, 
+            Optional<bool> ShowLegend = default, 
+            Optional<double> Opacity = default, 
+            Optional<TextType> Text = default, 
+            Optional<IEnumerable<TextType>> MultiText = default, 
+            Optional<StyleParam.Colorscale> ColorScale = default, 
+            Optional<bool> ShowScale = default, 
+            Optional<ColorBar> ColorBar = default, 
+            Optional<StyleParam.ConeSizeMode> SizeMode = default, 
+            Optional<StyleParam.ConeAnchor> ConeAnchor = default, 
+            Optional<bool> UseDefaults = default
+        )
+            where XType : IConvertible
+            where YType : IConvertible
+            where ZType : IConvertible
+            where UType : IConvertible
+            where VType : IConvertible
+            where WType : IConvertible
+            where TextType : IConvertible
+            =>
+                Plotly.NET.Chart3D.Chart.Cone<XType, YType, ZType, UType, VType, WType, TextType>(
+                    x: x,
+                    y: y,
+                    z: z,
+                    u: u,
+                    v: v,
+                    w: w,
+                    Name: Name.ToOption(),
+                    ShowLegend: ShowLegend.ToOption(),
+                    Opacity: Opacity.ToOption(),
+                    Text: Text.ToOption(),
+                    MultiText: MultiText.ToOption(),
+                    ColorScale: ColorScale.ToOption(),
+                    ShowScale: ShowScale.ToOption(),
+                    ColorBar: ColorBar.ToOption(),
+                    SizeMode: SizeMode.ToOption(),
+                    ConeAnchor: ConeAnchor.ToOption(),
+                    UseDefaults: UseDefaults.ToOption()
+                );
     }
 }
