4420d1424c8d3b9c1917715861369fa0f5892327
Kevin Schneider <schneider.kev@outlook.de>
2022-08-19 13:50:36 +0200

    Add C# bindings for Chart.LineGeo

--- src/Plotly.NET.CSharp/ChartAPI/ChartMap.cs
+++ src/Plotly.NET.CSharp/ChartAPI/ChartMap.cs
@@ -124,7 +124,7 @@ namespace Plotly.NET.CSharp
             Optional<StyleParam.DrawingStyle> LineDash = default,
             Optional<Line> Line = default,
             Optional<StyleParam.LocationFormat> LocationMode = default,
-            Optional<Object> GeoJson = default,
+            Optional<object> GeoJson = default,
             Optional<string> FeatureIdKey = default,
             Optional<bool> UseDefaults = default
         )
@@ -204,7 +204,7 @@ namespace Plotly.NET.CSharp
             Optional<IEnumerable<StyleParam.MarkerSymbol>> MultiMarkerSymbol = default,
             Optional<Marker> Marker = default,
             Optional<StyleParam.LocationFormat> LocationMode = default,
-            Optional<Object> GeoJson = default,
+            Optional<object> GeoJson = default,
             Optional<string> FeatureIdKey = default,
             Optional<bool> UseDefaults = default
         )
@@ -229,6 +229,98 @@ namespace Plotly.NET.CSharp
                     MarkerSymbol: MarkerSymbol.ToOption(),
                     MultiMarkerSymbol: MultiMarkerSymbol.ToOption(),
                     Marker: Marker.ToOption(),
+                    LocationMode: LocationMode.ToOption(),
+                    GeoJson: GeoJson.ToOption(),
+                    FeatureIdKey: FeatureIdKey.ToOption(),
+                    UseDefaults: UseDefaults.ToOption()
+                );
+
+        /// <summary>
+        /// Creates a LineGeo chart.
+        ///
+        /// In general, LineGeo Plots plot two-dimensional data connected by lines using plotly's base geo map.
+        /// </summary>
+        /// <param name="longitudes">Sets the longitude coordinates (in degrees East).</param>
+        /// <param name="latitudes">Sets the latitude coordinates (in degrees North).</param>
+        /// <param name="ShowMarkers"></param>
+        /// <param name="Name">Sets the trace name. The trace name appear as the legend item and on hover</param>
+        /// <param name="ShowLegend">Determines whether or not an item corresponding to this trace is shown in the legend.</param>
+        /// <param name="Opacity">Sets the opactity of the trace</param>
+        /// <param name="MultiOpacity">Sets the opactity of individual datum markers</param>
+        /// <param name="Text">Sets a text associated with each datum</param>
+        /// <param name="MultiText">Sets individual text for each datum</param>
+        /// <param name="TextPosition">Sets the position of text associated with each datum</param>
+        /// <param name="MultiTextPosition">Sets the position of text associated with individual datum</param>
+        /// <param name="MarkerColor">Sets the color of the marker</param>
+        /// <param name="MarkerColorScale">Sets the colorscale of the marker</param>
+        /// <param name="MarkerOutline">Sets the outline of the marker</param>
+        /// <param name="MarkerSymbol">Sets the marker symbol for each datum</param>
+        /// <param name="MultiMarkerSymbol">Sets the marker symbol for each individual datum</param>
+        /// <param name="Marker">Sets the marker (use this for more finegrained control than the other marker-associated arguments)</param>
+        /// <param name="LineColor">Sets the color of the line</param>
+        /// <param name="LineColorScale">Sets the colorscale of the line</param>
+        /// <param name="LineWidth">Sets the width of the line</param>
+        /// <param name="LineDash">sets the drawing style of the line</param>
+        /// <param name="Line">Sets the line (use this for more finegrained control than the other line-associated arguments)</param>
+        /// <param name="LocationMode">Determines the set of locations used to match entries in `locations` to regions on the map. Values "ISO-3", "USA-states", "country names" correspond to features on the base map and value "geojson-id" corresponds to features from a custom GeoJSON linked to the `geojson` attribute.</param>
+        /// <param name="GeoJson">Sets optional GeoJSON data associated with this trace. If not given, the features on the base map are used. It can be set as a valid GeoJSON object or as a URL string. Note that we only accept GeoJSONs of type "FeatureCollection" or "Feature" with geometries of type "Polygon" or "MultiPolygon".</param>
+        /// <param name="FeatureIdKey">Sets the key in GeoJSON features which is used as id to match the items included in the `locations` array. Only has an effect when `geojson` is set. Support nested property, for example "properties.name".</param>
+        /// <param name="UseDefaults">If set to false, ignore the global default settings set in `Defaults`</param>
+        public static GenericChart.GenericChart LineGeo<LongitudesType, LatitudesType, TextType>(
+            IEnumerable<LongitudesType> longitudes,
+            IEnumerable<LatitudesType> latitudes,
+            Optional<bool> ShowMarkers = default,
+            Optional<string> Name = default,
+            Optional<bool> ShowLegend = default,
+            Optional<double> Opacity = default,
+            Optional<IEnumerable<double>> MultiOpacity = default,
+            Optional<TextType> Text = default,
+            Optional<IEnumerable<TextType>> MultiText = default,
+            Optional<StyleParam.TextPosition> TextPosition = default,
+            Optional<IEnumerable<StyleParam.TextPosition>> MultiTextPosition = default,
+            Optional<Color> MarkerColor = default,
+            Optional<StyleParam.Colorscale> MarkerColorScale = default,
+            Optional<Line> MarkerOutline = default,
+            Optional<StyleParam.MarkerSymbol> MarkerSymbol = default,
+            Optional<IEnumerable<StyleParam.MarkerSymbol>> MultiMarkerSymbol = default,
+            Optional<Marker> Marker = default,
+            Optional<Color> LineColor = default,
+            Optional<StyleParam.Colorscale> LineColorScale = default,
+            Optional<double> LineWidth = default,
+            Optional<StyleParam.DrawingStyle> LineDash = default,
+            Optional<Line> Line = default,
+            Optional<StyleParam.LocationFormat> LocationMode = default,
+            Optional<object> GeoJson = default,
+            Optional<string> FeatureIdKey = default,
+            Optional<bool> UseDefaults = default
+        )
+            where LongitudesType : IConvertible
+            where LatitudesType : IConvertible
+            where TextType : IConvertible
+            =>
+                Plotly.NET.ChartMap.Chart.LineGeo<LongitudesType, LatitudesType, TextType>(
+                    longitudes: longitudes,
+                    latitudes: latitudes,
+                    ShowMarkers: ShowMarkers.ToOption(),
+                    Name: Name.ToOption(),
+                    ShowLegend: ShowLegend.ToOption(),
+                    Opacity: Opacity.ToOption(),
+                    MultiOpacity: MultiOpacity.ToOption(),
+                    Text: Text.ToOption(),
+                    MultiText: MultiText.ToOption(),
+                    TextPosition: TextPosition.ToOption(),
+                    MultiTextPosition: MultiTextPosition.ToOption(),
+                    MarkerColor: MarkerColor.ToOption(),
+                    MarkerColorScale: MarkerColorScale.ToOption(),
+                    MarkerOutline: MarkerOutline.ToOption(),
+                    MarkerSymbol: MarkerSymbol.ToOption(),
+                    MultiMarkerSymbol: MultiMarkerSymbol.ToOption(),
+                    Marker: Marker.ToOption(),
+                    LineColor: LineColor.ToOption(),
+                    LineColorScale: LineColorScale.ToOption(),
+                    LineWidth: LineWidth.ToOption(),
+                    LineDash: LineDash.ToOption(),
+                    Line: Line.ToOption(),
                     LocationMode: LocationMode.ToOption(),
                     GeoJson: GeoJson.ToOption(),
                     FeatureIdKey: FeatureIdKey.ToOption(),
