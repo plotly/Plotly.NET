086b7301b0796de41b473eb25e9c41c6ea6c067e
Kevin Schneider <schneider.kev@outlook.de>
2022-08-22 17:21:44 +0200

    Add C# binding for Chart.PointCarpet

--- src/Plotly.NET.CSharp/ChartAPI/ChartCarpet.cs
+++ src/Plotly.NET.CSharp/ChartAPI/ChartCarpet.cs
@@ -181,6 +181,75 @@ namespace Plotly.NET.CSharp
                 UseDefaults: UseDefaults.ToOption()
             );
 
+        /// <summary>
+        /// Creates a point plot that lies on a specified carpet.
+        ///
+        /// In general, PointCarpet creates a point plot that uses the given carpet identifier as coordinate system.
+        ///
+        /// Wether the resulting plot is a cheater or true carpet plot depends on the referenced carpet.
+        /// </summary>
+        /// <param name="a">Sets the a-axis coordinates on the carpet.</param>
+        /// <param name="b">Sets the b-axis coordinates on the carpet.</param>
+        /// <param name="carpetAnchorId">The identifier of the carpet that this trace will lie on.</param>
+        /// <param name="Name">Sets the trace name. The trace name appear as the legend item and on hover</param>
+        /// <param name="ShowLegend">Determines whether or not an item corresponding to this trace is shown in the legend.</param>
+        /// <param name="Opacity">Sets the opactity of the trace</param>
+        /// <param name="MultiOpacity">Sets the opactity of individual datum markers</param>
+        /// <param name="Text">Sets a text associated with each datum</param>
+        /// <param name="MultiText">Sets individual text for each datum</param>
+        /// <param name="TextPosition">Sets the position of text associated with each datum</param>
+        /// <param name="MultiTextPosition">Sets the position of text associated with individual datum</param>
+        /// <param name="MarkerColor">Sets the color of the marker</param>
+        /// <param name="MarkerColorScale">Sets the colorscale of the marker</param>
+        /// <param name="MarkerOutline">Sets the outline of the marker</param>
+        /// <param name="MarkerSymbol">Sets the marker symbol for each datum</param>
+        /// <param name="MultiMarkerSymbol">Sets the marker symbol for each individual datum</param>
+        /// <param name="Marker">Sets the marker (use this for more finegrained control than the other marker-associated arguments)</param>
+        /// <param name="UseDefaults">If set to false, ignore the global default settings set in `Defaults`</param>
+        public static GenericChart.GenericChart PointCarpet<AType, BType, TextType>(
+            IEnumerable<AType> a,
+            IEnumerable<BType> b,
+            string carpetAnchorId,
+            Optional<string> Name = default,
+            Optional<bool> ShowLegend = default,
+            Optional<double> Opacity = default,
+            Optional<IEnumerable<double>> MultiOpacity = default,
+            Optional<TextType> Text = default,
+            Optional<IEnumerable<TextType>> MultiText = default,
+            Optional<StyleParam.TextPosition> TextPosition = default,
+            Optional<IEnumerable<StyleParam.TextPosition>> MultiTextPosition = default,
+            Optional<Color> MarkerColor = default,
+            Optional<StyleParam.Colorscale> MarkerColorScale = default,
+            Optional<Line> MarkerOutline = default,
+            Optional<StyleParam.MarkerSymbol> MarkerSymbol = default,
+            Optional<IEnumerable<StyleParam.MarkerSymbol>> MultiMarkerSymbol = default,
+            Optional<Marker> Marker = default,
+            Optional<bool> UseDefaults = default
+        )
+            where AType : IConvertible
+            where BType : IConvertible
+            where TextType : IConvertible
+        =>
+            Plotly.NET.ChartCarpet.Chart.PointCarpet<AType, BType, TextType>(
+                a: a,
+                b: b,
+                carpetAnchorId: carpetAnchorId,
+                Name: Name.ToOption(),
+                ShowLegend: ShowLegend.ToOption(),
+                Opacity: Opacity.ToOption(),
+                MultiOpacity: MultiOpacity.ToOption(),
+                Text: Text.ToOption(),
+                MultiText: MultiText.ToOption(),
+                TextPosition: TextPosition.ToOption(),
+                MultiTextPosition: MultiTextPosition.ToOption(),
+                MarkerColor: MarkerColor.ToOption(),
+                MarkerColorScale: MarkerColorScale.ToOption(),
+                MarkerOutline: MarkerOutline.ToOption(),
+                MarkerSymbol: MarkerSymbol.ToOption(),
+                MultiMarkerSymbol: MultiMarkerSymbol.ToOption(),
+                Marker: Marker.ToOption(),
+                UseDefaults: UseDefaults.ToOption()
+            );
 
         /// <summary>
         /// Creates a contour chart that lies on a specified carpet.
