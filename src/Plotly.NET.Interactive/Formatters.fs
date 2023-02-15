c1704d87709e79b5359f00f647a32785ea31a9ad
Kevin Schneider <schneider.kev@outlook.de>
2023-02-15 12:53:59 +0100

    Use new DisplayOptions functions to use requirejs with the Global plotlyjs version in Plotly.NET.Interactive

--- src/Plotly.NET.Interactive/Formatters.fs
+++ src/Plotly.NET.Interactive/Formatters.fs
@@ -7,4 +7,6 @@ module Formatters =
 
     /// Converts a GenericChart to it's HTML representation and embeds it in a div element, together with the chart description for display in notebook environments.
     let toInteractiveHTML gChart =
-        toChartHTML gChart
+        gChart
+        |> Chart.withDisplayOptionsStyle(PlotlyJSReference = Require $"https://cdn.plot.ly/plotly-{Globals.PLOTLYJS_VERSION}.min")
+        |> GenericChart.toChartHTML
