f420b1f4c123baf61bb5aa8b1d00020c30fff1ee
Kevin Schneider <schneider.kev@outlook.de>
2023-03-21 09:28:58 +0100

    #334: port simple html tests

---
+++ tests/Plotly.NET.CSharp.Tests/TestUtils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicObj;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.IO;
+using Xunit;
+
+namespace CSharp.Tests
+{
+    internal class TestUtils
+    {
+        static string GetFullPlotlyJS()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            using Stream str = assembly.GetManifestResourceStream("Plotly.NET.Tests.plotly-2.18.1.min.js");
+            using StreamReader r = new StreamReader(str);
+            return r.ReadToEnd();
+        }
+
+    //A method that takes a Generic chart as input, transforms it with a delegate called 'htmlizer' into a string, for which it then should be tested wether another string is contained.
+    internal static void SubstringIsInChart(Plotly.NET.GenericChart.GenericChart chart, Func<Plotly.NET.GenericChart.GenericChart, string> htmlizer, string expected) 
+        {
+            string actual = htmlizer(chart);
+            Assert.Contains(expected, actual);
+        }
+
+
+    internal static void ChartGeneratedContains(Plotly.NET.GenericChart.GenericChart chart, string expected) 
+        {
+            SubstringIsInChart(chart, Plotly.NET.GenericChart.toChartHTML, expected);
+            SubstringIsInChart(chart, Plotly.NET.GenericChart.toEmbeddedHTML, expected);
+        }
+
+    //C# version of the following F# code:
+    //let substringListIsInChart chart htmlizer substringList =
+    //    for substring in substringList do
+    //        substringIsInChart chart htmlizer substring
+    internal static void SubstringListIsInChart(Plotly.NET.GenericChart.GenericChart chart, Func<Plotly.NET.GenericChart.GenericChart, string> htmlizer, string[] substringList)
+        {
+            foreach (string substring in substringList)
+            {
+                SubstringIsInChart(chart, htmlizer, substring);
+            }
+        }
+
+    //C# verison of the following F# code:
+    //let chartGeneratedContainsList chart substringList =
+    //for substring in substringList do
+    //    chartGeneratedContains chart substring
+    internal static void ChartGeneratedContainsList(Plotly.NET.GenericChart.GenericChart chart, string[] substringList)
+        {
+            foreach (string substring in substringList)
+            {
+                ChartGeneratedContains(chart, substring);
+            }
+        }
+
+    }
+}
---
+++ tests/Plotly.NET.CSharp.Tests/htmlcodegen/SimpleTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+using Plotly.NET.CSharp;
+
+// avoid the namespace 'Plotly.NET', as we dont want to accidentally use F# type extensions instead of real C# bindings
+namespace CSharp.Tests.HtmlCodegen
+{
+    public class SimpleTests
+    {
+        static Plotly.NET.GenericChart.GenericChart simpleChart =
+            Chart.Point<double, double, string>(
+                x: Enumerable.Range(0, 11).Select(x => Convert.ToDouble(x)).ToArray(),
+                y: Enumerable.Range(0, 11).Select(x => Convert.ToDouble(x)).ToArray(),
+                UseDefaults: false
+            )
+                .WithXAxisStyle<int, int, int>(TitleText: "xAxis")
+                .WithYAxisStyle<int, int, int>(TitleText: "yAxis");
+
+        public class HtmlLayoutTests
+        {
+            [Fact]
+            public void PlotlyJsScriptInGeneratedHTMLDocument()
+            {
+                TestUtils.SubstringIsInChart(
+                    simpleChart,
+                    Plotly.NET.GenericChart.toEmbeddedHTML, 
+                    $"https://cdn.plot.ly/plotly-{Globals.PLOTLYJS_VERSION}.min"
+                );
+            }
+
+            [Fact]
+            public void ExpectingData()
+            {
+                TestUtils.ChartGeneratedContains(
+                    simpleChart, 
+                    """var data = [{"type":"scatter","mode":"markers","x":[0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0],"y":[0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0],"marker":{},"line":{}}];"""
+                );
+            }
+
+            [Fact]
+            public void ExpectingLayoutInfo()
+            {
+                TestUtils.ChartGeneratedContains(
+                    simpleChart, 
+                    """var layout = {"xaxis":{"title":{"text":"xAxis"}},"yaxis":{"title":{"text":"yAxis"}}};"""
+                );
+            }
+
+            [Fact]
+            public void ExpectingHTMLTagsInEmbeddedPageOnly()
+            {
+                string[] tags = new string[] { "<html>", "</html>", "<head>", "</head>", "<body>", "</body>", "<script type=\"text/javascript\">", "</script>" };
+                TestUtils.SubstringListIsInChart(
+                    simpleChart,
+                    Plotly.NET.GenericChart.toEmbeddedHTML,
+                    tags
+                );
+            }
+
+            [Fact]
+            public void ExpectingSomeHTMLTagsInBothEmbeddedAndNotEmbedded()
+            {
+                string[] tags = new string[] { "<script type=\"text/javascript\">", "</script>" };
+                TestUtils.ChartGeneratedContainsList(
+                    simpleChart,
+                    tags
+                );
+            }
+
+            [Fact]
+            public void PassingArgsToTheFunction()
+            {
+                TestUtils.ChartGeneratedContains(
+                    simpleChart,
+                    "data, layout, config);"
+                );
+            }
+
+        }
+    }
+}
